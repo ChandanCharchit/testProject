1035e9efc8eea9ca6fdf15492994aefee463bbb2
Chandan Charchit Sahoo <74564556+ChandanCharchit@users.noreply.github.com>
2023-09-15 17:33:56 +0530

    Update pandastest.py

--- pandastest.py
+++ pandastest.py
@@ -10,7 +10,7 @@ class Mix:
         print("coffee shake is made with {} coffee and {} sugar.".format(self.coffee, self.sugar))
 
 
-mix1 = Mix(2, 1)
+mix1 = Mix(2, 6)
 mix2 = Mix(2, 4)
 
 mix3  = mix1+mix2
