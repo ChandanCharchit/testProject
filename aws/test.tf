c231b5740a7ab260037f60a9edc019d031b2fb3f
Chandan Charchit Sahoo <74564556+ChandanCharchit@users.noreply.github.com>
2023-12-06 12:19:48 +0530

    Update test.tf

--- aws/test.tf
+++ aws/test.tf
@@ -11,3 +11,23 @@ resource "aws_s3_bucket" "example" {
        http_tokens   = "required"
   }
 }
+
+resource "aws_s3_bucket" "good_example" {
+	bucket = "abcdefgh"
+	acl = "private"
+}
+
+resource "aws_cloudtrail" "foobar" {
+  name                          = "tf-trail-foobar"
+  s3_bucket_name                = aws_s3_bucket.foo.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
++ is_multi_region_trail = true
+}
+
+resource "aws_flow_log" "example" {
+  iam_role_arn    = "arn"
+  log_destination = "log"
+  traffic_type    = "ALL"
++ vpc_id          = aws_vpc.ok_vpc.id
+}
